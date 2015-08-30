d41b9269a0da9bd8fcf11628ea525a7d4bcfc245
Frank Zigan <Frank.Zigan@uni-weimar.de>
2015-08-30 23:43:32 +0200

    - added functions to camera

---
+++ framework/camera.cpp
@@ -0,0 +1,44 @@
+#include "camera.hpp"
+
+//ctor dtor
+Camera::Camera()
+: _position{0.0},
+  _direction{0.0, 0.0, -1.0}
+{
+	//implement me!
+}
+
+Camera::Camera(glm::vec3 const& direction, float fov)
+: _direction{direction},
+  _fov{fov}
+{
+	//implement me!
+}
+
+Camera::Camera(std::string const& name, float fov)
+: _name(name),
+  _fov(fov)
+{
+	//implement me!
+}
+
+Camera::~Camera()
+{}
+
+
+//get-methods
+
+std::string const& Camera::name() const
+{
+	return _name;
+}
+
+glm::vec3 const& Camera::position() const
+{
+	return _position;
+}
+
+glm::vec3 const& Camera::direction() const
+{
+	return _direction;
+}
---
+++ framework/camera.hpp
@@ -0,0 +1,31 @@
+#ifndef CAMERA_HPP
+#define CAMERA_HPP
+#include <iostream>
+#include <glm/vec3.hpp>
+
+class Camera
+{
+	public:
+			// ctors dtors
+			Camera();
+			Camera(glm::vec3 const&, float direction);
+			Camera(std::string const& name, float fov); 
+			~Camera();
+
+			// get-methods
+			std::string const& name() const;
+			glm::vec3 const& position() const;
+			glm::vec3 const& direction() const;
+
+	private:
+			glm::vec3 _position;
+			glm::vec3 _direction;
+			std::string _name;
+			float _fov; // fov = field of view
+			glm::vec3 upVector;
+
+			
+};
+
+
+#endif
